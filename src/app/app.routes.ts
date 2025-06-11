ea61080180442ed3879b3f059676369bb10f0702
Roshan <chawalroshan@gmail.com>
2025-06-11 11:51:19 +0545

    create login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Layout } from './pages/layout/layout';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Employee } from './pages/employee/employee';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'',
+        redirectTo:'login',
+        pathMatch:'full'
+
+    },
+
+    {
+        path:'login',
+        component:Login
+
+    },
+
+    {
+        path:'',
+        component:Layout,
+        children:[
+            {
+                path:'dashboard',
+                component:Dashboard
+            },
+            {
+                path:'employee',
+                component:Employee
+            }
+        ]
+    }
+
+
+
+
+];
---
+++ src/app/pages/project-employee/project-employee.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-employee',
+  imports: [],
+  templateUrl: './project-employee.html',
+  styleUrl: './project-employee.css'
+})
+export class ProjectEmployee {
+
+}
